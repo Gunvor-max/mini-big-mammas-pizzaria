43e99faf97c6e2c8faa43573907e9d5135264ebd
Frederik Rasmussen <wtfhaxxorone@gmail.com>
2023-10-30 13:46:49 +0100

    Oprettet drikkevarer class

--- Model/Drikkevarer.cs
+++ Model/Drikkevarer.cs
@@ -2,5 +2,28 @@
 {
     public class Drikkevarer
     {
+        //properties
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        
+        //constructor
+        public Drikkevarer()
+        {
+            Name = "";
+            Description = "";
+            Price = 0;
+        }
+        public Drikkevarer(string name, string description, double price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Name)}={Name}, {nameof(Description)}={Description}, {nameof(Price)}={Price.ToString()}}}";
+        }
     }
 }
