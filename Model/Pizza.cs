3cb3a65ef710d1656e32d0889f7dd9efee2317fa
Frederik Rasmussen <wtfhaxxorone@gmail.com>
2023-10-30 13:59:23 +0100

    Created pizza class

--- Model/Pizza.cs
+++ Model/Pizza.cs
@@ -2,5 +2,30 @@
 {
     public class Pizza
     {
+        //Properties
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+
+
+        //Constructor
+        public Pizza(string name, int number, string description, double price)
+        {
+            Name = name;
+            Number = number;
+            Description = description;
+            Price = price;
+
+        }
+
+
+        public override string ToString()
+        {
+
+            return $"[{Name}, {Number}, {Price}kr.]";
+        }
+
+
     }
 }
