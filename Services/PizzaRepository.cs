98cfaf402e401e1f67d0c49449fd336aef073663
Mathias Birkkjær Nielsen <birkkjaer6@gmail.com>
2023-10-31 14:50:28 +0100

    Services

--- Services/PizzaRepository.cs
+++ Services/PizzaRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace mini_big_mammas_pizzaria.Services
+﻿using mini_big_mammas_pizzaria.Model;
+
+namespace mini_big_mammas_pizzaria.Services
 {
     public class PizzaRepository
     {
+        //property
+        public Dictionary<int, Pizza> Menukort = new Dictionary<int, Pizza>();
+
+        //constructor
+        public PizzaRepository()
+        {
+            Menukort = new Dictionary<int, Pizza>();
+        }
+
+
     }
 }
