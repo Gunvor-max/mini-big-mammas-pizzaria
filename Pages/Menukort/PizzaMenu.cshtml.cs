c14d5818fb5e0f516536f316e51b5df834504cb0
Mathias Birkkjær Nielsen <birkkjaer6@gmail.com>
2023-10-31 16:44:33 +0100

    s

--- Pages/Menukort/PizzaMenu.cshtml.cs
+++ Pages/Menukort/PizzaMenu.cshtml.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using mini_big_mammas_pizzaria.Model;
+using mini_big_mammas_pizzaria.Services;
 
 namespace mini_big_mammas_pizzaria.Pages.Menukort
 {
     public class PizzaMenuModel : PageModel
     {
+        //Property
+        public List<Pizza> PizzaMenukort {  get; set; }
+        
         public void OnGet()
         {
+            PizzaRepository repo = new PizzaRepository();
+            PizzaMenukort = repo.GetAllItems();
         }
     }
 }
