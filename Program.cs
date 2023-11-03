1cb8608fa24bbd53a875ea46c0751fe2c90f7fca
Mathias Birkkjær Nielsen <birkkjaer6@gmail.com>
2023-11-03 22:06:31 +0100

    Arbejder på login

--- Program.cs
+++ Program.cs
@@ -1,8 +1,23 @@
 using mini_big_mammas_pizzaria.Model;
 using mini_big_mammas_pizzaria.Services;
+using System.Security.Cryptography.X509Certificates;
 
 internal class Program
 {
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", options =>
+        {
+            options.Cookie.Name = "MyCookieAuth";
+        });
+
+        services.AddAuthorization(options =>
+        {
+            services.Add("MustBelongToAdmin",
+               policy => policy.RequireClaim("Pizzaria"));
+        });
+        
+    }
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +28,7 @@ internal class Program
         //Indsætter pizzarepository
         builder.Services.AddSingleton<PizzaRepository>(new PizzaRepository());
         builder.Services.AddSingleton<Menucard>(new Menucard());
-
+        
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
